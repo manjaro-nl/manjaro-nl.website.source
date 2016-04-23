b84c4ee6acb8d20ff1216d609866b39350c06179
flipper-maniac <wormfl@zoho.com>
2016-04-23 18:13:51 +0200

    changing url to dev1

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://manjaro-nl.github.io'
+SITEURL = 'http://vd4493.dev1.novation.be'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
